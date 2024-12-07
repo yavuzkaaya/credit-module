7bf7cf363ba02eacb84b0910c5544658d4a01b00
vovo <kyavuz96@gmail.com>
2024-12-07 16:30:29 +0300

    dev: project initial structure and dependencies added

---
+++ src/main/java/com/example/creditmodule/CreditModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.creditmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/creditmodule/CreditModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.creditmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
